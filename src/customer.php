e0e54b11dd5b26cb60fa3194c408f1e6d271690d
Wilbert Anderson <wilbertmdp@gmail.com>
2025-08-29 22:18:58 +0700

    Contoh Library Sederhana Composer

---
+++ src/customer.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace programmerzamannow\Belajar;
+
+class customer{
+
+    public function __construct(private string $name){
+
+    }
+
+    public function sayhello(string $name):string{
+        return "hello $name, my name is $this->name";
+    }
+}
